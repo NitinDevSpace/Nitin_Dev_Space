a2409d39ec299562cc1dd0d10af7123e56172cbf
Nitin <nitindevspace@gmail.com>
2025-07-16 03:17:43 +0530

    Home

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,25 @@
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Admin from "./pages/Admin";
+import Blogs from "./pages/Blogs";
+import Contact from "./pages/Contact";
+import Feedback from "./pages/Feedback";
+import Projects from "./pages/Projects";
 
 function App() {
-
-  return (
-		<>
-			<div className="min-h-screen bg-gray-900 text-white">
-				<h1 className="text-3xl font-bold p-8">Nitin Dev Space</h1>
-			</div>
-		</>
+	return (
+		<Routes>
+			<Route path="/" element={<Home />} />
+			<Route path="/about" element={<About />} />
+			<Route path="/admin" element={<Admin />} />
+			<Route path="/blogs" element={<Blogs />} />
+			<Route path="/contact" element={<Contact />} />
+			<Route path="/feedback" element={<Feedback />} />
+			<Route path="/projects" element={<Projects />} />
+		</Routes>
 	);
 }
 
-export default App
+export default App;
--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
 
-const home = () => {
-  return (
-    <div>home</div>
-  )
-}
+const Home = () => {
+	return (
+		<div className="min-h-screen flex items-center bg-gradient-to-br from-accent1 via-primary to-accent2">
+			<div className="w-full max-w-content px-4">
+				<h1 className="text-4xl md:text-6xl font-heading text-center mb-8">
+					Nitin Dev Space
+				</h1>
+				<p className="text-center text-lg md:text-xl font-body">
+					Crafting interactive, performant experiences.
+				</p>
+				{/* 3D Centerpiece will go here */}
+			</div>
+		</div>
+	);
+};
 
-export default home
+export default Home;
