7cfc589b8c962031594fcf1576121136b426d501
Nitin <nitindevspace@gmail.com>
2025-07-22 01:40:49 +0530

    forms

--- client/src/components/Admin/AlittleAboutMe.jsx
+++ client/src/components/Admin/AlittleAboutMe.jsx
@@ -1,10 +1,113 @@
-import React from "react";
+import { ArrowDown } from "lucide-react";
+import React, { useState } from "react";
 
 function AlittleAboutMe() {
+	const [open, setOpen] = useState(false); 
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		const data = new FormData(e.target);
+		const payload = {
+			para: data.get("para"),
+			frontend: data.get("frontend"),
+			backend: data.get("backend"),
+			ai: data.get("ai"),
+			// image: data.get("image"),
+		};
+		console.log("Form Submitted", payload);
+	};
+
 	return (
-        <div className="bg-primary2 rounded-lg h-fit m-4 p-4">
-            AlittleAboutMe
-        </div>
+		<div className="bg-primary2 flex flex-col justify-center rounded-lg h-fit m-4 p-4">
+			{/* Heading Intro */}
+			<div className="flex">
+				<button
+					onClick={() => {
+						setOpen(!open);
+					}}
+				>
+					<ArrowDown />{" "}
+				</button>
+				<h1 className="p-6 text-2xl border-b-2">A Little About me</h1>
+			</div>
+			{/* Form & para Collapsible */}
+			{open && (
+				<div className="flex m-6 flex-col">
+					<form onSubmit={handleSubmit} method="post" className="mt-6">
+						<div className="flex flex-col">
+							<label htmlFor="para" className="text-xl">
+								Para:
+							</label>
+							<textarea
+								className="dark-input"
+								rows="3"
+								onInput={(e) => {
+									e.target.style.height = "auto"; // reset height
+									e.target.style.height = `${e.target.scrollHeight}px`; // set to full scroll height
+								}}
+								name="para"
+								id="para"
+								placeholder="Enter About Yourself"
+							/>
+						</div>
+						<div className="flex flex-col gap-2">
+							<label htmlFor="frontend" className="text-xl">
+								Frontend:
+							</label>
+							<textarea
+								className="dark-input"
+								rows="3"
+								onInput={(e) => {
+									e.target.style.height = "auto"; // reset height
+									e.target.style.height = `${e.target.scrollHeight}px`; // set to full scroll height
+								}}
+								name="frontend"
+								id="frontend"
+								placeholder="Enter Frontend Para"
+							/>
+						</div>
+						<div className="flex flex-col gap-2">
+							<label htmlFor="backend" className="text-xl">
+								Backend:
+							</label>
+							<textarea
+								className="dark-input"
+								rows="3"
+								onInput={(e) => {
+									e.target.style.height = "auto"; // reset height
+									e.target.style.height = `${e.target.scrollHeight}px`; // set to full scroll height
+								}}
+								name="backend"
+								id="backend"
+								placeholder="Enter Backend Para"
+							/>
+						</div>
+						<div className="flex flex-col gap-2">
+							<label htmlFor="ai" className="text-xl">
+								Ai:
+							</label>
+							<textarea
+								className="dark-input"
+								rows="3"
+								onInput={(e) => {
+									e.target.style.height = "auto"; // reset height
+									e.target.style.height = `${e.target.scrollHeight}px`; // set to full scroll height
+								}}
+								name="ai"
+								id="ai"
+								placeholder="Enter Ai Para"
+							/>
+						</div>
+						<button
+							type="submit"
+							className="p-2 bg-pink shadow-2xl rounded-lg mt-4 justify-self-center flex"
+						>
+							Update
+						</button>
+					</form>
+				</div>
+			)}
+		</div>
 	);
 }
 
--- client/src/components/Admin/Intro.jsx
+++ client/src/components/Admin/Intro.jsx
@@ -1,10 +1,74 @@
-import React from "react";
+import { ArrowDown } from "lucide-react";
+import React, { useState } from "react";
 
 function Intro() {
+	const [open, setOpen] = useState(false);
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		const data = new FormData(e.target);
+		const payload = {
+			imageUrl: data.get("imageUrl"),
+			bio: data.get("bio"),
+			// image: data.get("image"),
+		}
+		console.log("Form Submitted", payload);
+		
+	}
+
 	return (
-		<div className="bg-primary2 flex flex-col justify-center items-center rounded-lg h-fit m-4 p-4">
-			<h1>Intro</h1>
-			<form action="sumbit">fo</form>
+		<div className="bg-primary2 flex flex-col justify-center rounded-lg h-fit m-4 p-4">
+			{/* Heading Intro */}
+			<div className="flex">
+				<button
+					onClick={() => {
+						setOpen(!open);
+					}}
+				>
+					<ArrowDown />{" "}
+				</button>
+				<h1 className="p-6 text-2xl border-b-2">Intro</h1>
+			</div>
+			{/* Form & para Collapsible */}
+			{open && (
+				<div className="flex m-6 flex-col">
+					<div>
+						<h1>Upload an Image</h1>
+						<div>Image Container</div>
+					</div>
+					<form onSubmit={handleSubmit} method="post" className="mt-6">
+						<div className="mb-4 flex items-center">
+							<label htmlFor="imageUrl" className="text-xl">
+								Image Url:
+							</label>
+							<input
+								type="text"
+								placeholder="Enter the image URL"
+								name="imageUrl"
+								id="imageUrl"
+								className="dark-input"
+							/>
+						</div>
+						<div className="flex flex-col gap-2">
+							<label htmlFor="bio" className="text-xl">
+								Bio:
+							</label>
+							<textarea
+								className="dark-input"
+								rows="3"
+								onInput={(e) => {
+									e.target.style.height = "auto"; // reset height
+									e.target.style.height = `${e.target.scrollHeight}px`; // set to full scroll height
+								}}
+								name="bio"
+								id="bio"
+								placeholder="Enter your bio"
+							/>
+						</div>
+						<button type="submit" className="p-2 bg-pink border rounded-lg mt-4 justify-self-center flex">Update</button>
+					</form>
+				</div>
+			)}
 		</div>
 	);
 }
