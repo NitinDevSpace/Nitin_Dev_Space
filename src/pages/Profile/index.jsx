c1759edf36b77fccadcf415e8f2e206397a80648
Nitin <nitindevspace@gmail.com>
2025-07-29 22:48:35 +0530

    starting About me Page

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
-  return (
-    <div>About</div>
-  )
-}
+	return (
+		<>
+			<section className="relative h-full flex items-center justify-center">
+        <div className=" ">
+          <h1>Hi, I'm Nitin Kumar and This is my Story</h1>
+        </div>
+			</section>
+		</>
+	);
+};
 
-export default About
+export default About;
