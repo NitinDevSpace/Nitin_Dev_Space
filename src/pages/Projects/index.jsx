2115d25d36f3e9b519c74b4b8695d002c1bc2fd8
Nitin <nitindevspace@gmail.com>
2025-08-07 21:29:09 +0530

    Projects page

--- src/pages/Projects/index.jsx
+++ src/pages/Projects/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import ProjectCard from '../../components/ProjectCard';
 
 const Projects = () => {
   return (
 		<>
 			<section className="relative h-full flex items-center justify-center">
 				<div className=" ">
-					<h1>For now Just go to my Creations section</h1>
+				  <h1>For now Just go to my Creations section</h1>
+				  <ProjectCard />
 					<img
 						src="https://media1.giphy.com/media/v1.Y2lkPTZjMDliOTUyaWczdnVyNTNvdzhhYzhkemd3c3liZWYwY2dvcnM2dWxhbDM5azN2bSZlcD12MV9naWZzX3NlYXJjaCZjdD1n/vR1dPIYzQmkRzLZk2w/source.gif"
 						className="w-full h-full"
