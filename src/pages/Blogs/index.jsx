398c1da6172ddcc404fc9394b540adbf88b0131b
Nitin <nitindevspace@gmail.com>
2025-09-28 17:48:00 +0530

    blog text removed

--- src/pages/Blogs/index.jsx
+++ src/pages/Blogs/index.jsx
@@ -5,7 +5,6 @@ const Blogs = () => {
 		<>
 			<section className="relative h-full flex items-center justify-center">
 				<div className=" flex flex-col gap-12 ">
-					<h1>Hi, I'm Nitin Kumar and I will write my thoughts here</h1>
 					<img
 						src="https://media1.giphy.com/media/v1.Y2lkPTZjMDliOTUyaWczdnVyNTNvdzhhYzhkemd3c3liZWYwY2dvcnM2dWxhbDM5azN2bSZlcD12MV9naWZzX3NlYXJjaCZjdD1n/vR1dPIYzQmkRzLZk2w/source.gif"
 						className="w-full h-full"
